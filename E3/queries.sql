5cf9bde46b7b836af96855be5065750f0d04d17c
Guilherme Lima <82712415+GLima727@users.noreply.github.com>
2022-06-18 23:23:22 +0100

    indexes

--- E3/queries.sql
+++ E3/queries.sql
@@ -59,3 +59,29 @@ where ean not in
 	(select ean
 	from evento_reposicao);
 
+
+
+
+/* 7.1
+Utilizamos um hash index no nome da categoria pelo facto de na consulta estar especificado o nome da categoria como 'Frutos',
+realizando a procura dos tins depois de ser obtido o Retalhista com categoria de 'Frutos, realizando a procura de forma mais rápida.*/
+drop index cat_idx
+
+create index cat_idx on responsavel_por using hash(nome_cat)
+
+SELECT DISTINCT R.nome
+FROM retalhista R, responsavel_por P
+WHERE R.tin = P.tin and P. nome_cat = 'Frutos'
+
+/* 7.2
+Utilizamos um index bi-tree como critério a descrição começar com a letra A porque assim descobre rapidamente os produtos
+com uma descrição com esse critério visto que já é conhecido, e evitamos usar um index hash visto que várias descrições podem
+começar com 'A' e ordenar alfabeticamente é mais rápido com uma binary tree. Depois de descobrir o produto rapidamente, irá comparar os nomes das categorias.*/
+drop index des_idx
+
+create index des_idx on produto(descr)
+
+SELECT T.nome, count(T.ean)
+FROM produto P, tem_categoria T
+WHERE p.cat = T.nome and P.desc like 'A%'
+GROUP BY T.nome

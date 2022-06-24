8ea1a12db70b359628e9f18b63b180edfce48331
Andrew1708 <andre.filipe.morgado@tecnico.ulisboa.pt>
2022-06-24 22:14:42 +0100

    Update ICs.sql

--- E3/ICs.sql
+++ E3/ICs.sql
@@ -1,74 +1,90 @@
 -- RI - 1
 
-drop trigger if exists tem_outra_trigger on tem_outra;  -- garante que não existe um trigger com o mesmo nome associado a tabela
+drop trigger if exists RI1 on tem_outra;  -- garante que não existe um trigger com o mesmo nome associado a tabela
 
-create or replace function tem_outra_trigger() return as $$ -- cria função
+create or replace function tem_outra_trigger() returns trigger as $$ -- cria função
 
 begin
-    if new.categoria == new.super_categoria then 
-        raise exception 'As Categorias não podem estar contidas dentro de si próprias' 
+    if categoria == super_categoria then 
+        raise exception 'As Categorias não podem estar contidas dentro de si próprias';
     end if;
-    return new;
 
+    WITH RECURSIVE cats AS(
+                 SELECT categoria
+                 FROM tem_outra
+                 WHERE super_categoria = new.super_categoria
+                 UNION
+                    SELECT t.categoria
+                    FROM tem_outra t
+                    INNER JOIN cats c on c.categoria = t.super_categoria
+                )
+    select categoria, super_categoria
+    from cats
+    where categoria = super_categoria;
+
+    if found then 
+        raise exception 'Existe circularidade de categorias';
+    end if;
+    return NEW;
+    
 end;
-$$ language plpgsql
+$$ language plpgsql;
 
-create trigger tem_outra_trigger before insert on tem_outra -- associa trigger a tabela
+create trigger RI1 before insert on tem_outra 
 for each row execute procedure tem_outra_trigger();
 
 
 -- RI - 4
-drop trigger if exists verifica_unidades on evento_reposicao;
+drop trigger if exists RI4 on evento_reposicao;
 
-create or replace function verifica_unidades() return as $$
+create or replace function verifica_unidades() returns trigger as $$
 -- variável que contem as unidades especificadas no planograma
-declare unidades_planograma numeric(10) 
+declare unidades_planograma numeric(10);
 
 begin
     -- vamos buscar essas unidades à tabela do planograma
     select unidades into unidades_planograma 
     from planograma
-    where new.ean = ean and new.nro = nro and new.num_serie = num_serie and new.fabricante = fabricante 
+    where new.ean = ean and new.nro = nro and new.num_serie = num_serie and new.fabricante = fabricante; 
     -- pk de planograma
 
     if new.unidades > unidades_planograma then
         raise exception 'O número de unidades repostas num Evento de Reposição
-                            não pode exceder o número de unidades especificado no Planograma'
+                            não pode exceder o número de unidades especificado no Planograma';
     end if;
     return new;
 
 end;
-$$ language plpgsql
+$$ language plpgsql;
 
-create trigger verifica_unidades before insert on evento_reposicao
+create trigger RI4 before insert on evento_reposicao
 for each row execute procedure verifica_unidades();
 
 -- RI - 5
-drop trigger if exists verifica_produto on evento_reposicao;
+drop trigger if exists RI5 on evento_reposicao;
 
-create or replace function verifica_produto() return as $$
-
-declare categoria_prod varchar(80)
+create or replace function verifica_produto() returns trigger as $$
 
 begin
     -- vai buscar a categoria do produto a repor
-    select cat into categoria_prod
+    perform cat
     from produto
-    where ean = new.ean
+    where ean = new.ean and cat 
+    in(
     -- tabela com as categorias da prateleira
-    (select nome
-    from prateleira
-    where new.nro = nro and new.num_serie = num_serie and new.fabricante = fabricante)
-    as categorias_prateleira -- É PRECISO DECLARAR??
+        select nome
+        from prateleira
+        where new.nro = nro and new.num_serie = num_serie and new.fabricante = fabricante
+        );
 
-    if categoria_prod not in categorias_prateleira then
+    if not found then
         raise exception 'Um Produto só pode ser reposto numa Prateleira que 
-                            apresente (pelo menos) uma das Categorias desse produto'
+                            apresente (pelo menos) uma das Categorias desse produto';
     end if;
     return new;
 
 end;
-$$ language plpgsql
+$$ language plpgsql;
 
-create trigger verifica_produto before insert on evento_reposicao
+create trigger RI5 before insert on evento_reposicao
 for each row execute procedure verifica_produto();
